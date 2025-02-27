c880cdd4291f23a5f2c72b2953f5715373f782fb
Jhonathang21 <sistemasareatecnica@gmail.com>
2025-02-27 12:44:02 -0500

    ajustamos a responsive borramos las fotos y ajustamos el contenido

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // Botón "Volver arriba"
-//const scrollTopBtn = document.getElementById("scrollTopBtn");
+const scrollTopBtn = document.getElementById("scrollTopBtn");
 const scrollTopNav = document.getElementById("scrollTopNav");
 
+ window.onscroll = () => {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     scrollTopBtn.style.display = "block";
+   } else {
+     scrollTopBtn.style.display = "none";
+   }
+ };
+
+ scrollTopBtn.addEventListener("click", () => {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ });
+
 // window.onscroll = () => {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     scrollTopBtn.style.display = "block";
-//   } else {
-//     scrollTopBtn.style.display = "none";
-//   }
-// };
-
-// scrollTopBtn.addEventListener("click", () => {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// });
-
-window.onscroll = () => {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      scrollTopNav.style.display = "block";
-    } else {
-      scrollTopNav.style.display = "none";
-    }
-  };
+//     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+//       scrollTopNav.style.display = "block";
+//     } else {
+//       scrollTopNav.style.display = "none";
+//     }
+//   };
